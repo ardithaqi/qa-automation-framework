0ebf5dd43459184776c9a98ddb8c47ead234f8d8
ardi-macbook <aardithaqi@gmail.com>
2026-02-27 19:13:57 +0100

    added new test case for testing purposes.

--- tests/examples/saucedemo/test.spec.ts
+++ tests/examples/saucedemo/test.spec.ts
@@ -3,5 +3,12 @@ import { expect } from "@playwright/test";
 
 test("forced regression", async ({ page }) => {
     await page.goto("https://www.saucedemo.com");
-    expect(1).toBe(2);
+});
+
+test("flaky regression", async ({ page }) => {
+    await page.goto("https://www.saucedemo.com");
+    await page.locator("#user-name").fill("standard_user");
+    await page.locator("#password").fill("secret_sauce");
+    await page.locator("#login-button").click();
+    await expect(page).toHaveURL(/inventory/);
 });
