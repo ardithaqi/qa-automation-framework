add95e99b468956d8d6f30bfc412d650db3bafcd
ardi-macbook <aardithaqi@gmail.com>
2026-02-27 15:53:12 +0100

    added one line to see the ai.txt on artifacts

--- tests/examples/saucedemo/test.spec.ts
+++ tests/examples/saucedemo/test.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("forced regression", async () => {
+test("forced regression", async ({ page }) => {
+    await page.goto("https://www.saucedemo.com");
     expect(1).toBe(2);
 });
