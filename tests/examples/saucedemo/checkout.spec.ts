11a83dd49fbcfe8fd8a1ea00cfabfbc82bc8825b
ardi-macbook <aardithaqi@gmail.com>
2026-02-27 15:25:24 +0100

    intentionally failing the test case

--- tests/examples/saucedemo/checkout.spec.ts
+++ tests/examples/saucedemo/checkout.spec.ts
@@ -23,4 +23,5 @@ test("user can checkout", async ({ page }) => {
 
     await page.locator("[data-test='add-to-cart-sauce-labs-fleece-jacket']").click();
 
+    expect(1).toBe(2); // intentional failure
 });
